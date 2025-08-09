13264cf60958767e3e1959583ff9090e1fafd32f
cerenozdgn <ceren.ozdogann98@gmail.com>
2025-08-09 14:39:54 +0300

    New Repository is added

---
+++ src/main/java/com/example/patientmanagementplatform/PatientManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.patientmanagementplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientManagementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/patientmanagementplatform/PatientManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patientmanagementplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
